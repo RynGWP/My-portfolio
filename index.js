fc6cb54e8c963ae46c4d9a56f6bf1169d496d9a5
RynGWP <120027550+RynGWP@users.noreply.github.com>
2024-07-29 23:43:02 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let job = document.querySelector(".job");
+
+const jobs = [
+   "Web Developer",
+   "Web Designer",
+   "Software Engineer",
+   "Programmer"
+];
+
+document.addEventListener("DOMContentLoaded" , function()  {
+    
+    
+    setInterval (function() {
+        
+    let random = Math.floor(Math.random() * jobs.length);
+    job.textContent = jobs[random];
+},5000);    
+
+
+});
